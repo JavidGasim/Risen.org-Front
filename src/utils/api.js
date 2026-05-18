817097769f4a6a160537b57ccfd45cbe0e5b9487
JavidGasim <gasimovcavid777@gmail.com>
2026-05-19 01:29:39 +0400

    api chandeg 2

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { getCookie } from './cookie';
 
 // Using the provided ASP.NET Core dev port
-const API_URL = import.meta.env.VITE_API_BASE_URL || 'https://RisenDB.mssql.somee.com/api';
+const API_URL = import.meta.env.VITE_API_BASE_URL || 'https://risen-org-back.onrender.com/api';
 
 const api = axios.create({
   baseURL: API_URL,
