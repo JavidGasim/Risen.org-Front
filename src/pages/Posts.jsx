0cbb72dba02934080abf665a82d61ab0db0c38e5
JavidGasim <gasimovcavid777@gmail.com>
2026-07-04 15:12:04 +0400

    .

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useState } from 'react';
+import { useEffect, useMemo, useState, useCallback } from 'react';
 import { useAuth } from '../context/AuthContext';
 import api from '../utils/api';
 import {
