15ce3df53403cd0b29ca26e87931df4945a08d3f
JavidGasim <gasimovcavid777@gmail.com>
2026-07-17 15:38:50 +0400

    .

--- src/hooks/useFriendSignalR.js
+++ src/hooks/useFriendSignalR.js
@@ -1,33 +1,32 @@
 import { useEffect } from "react";
-import { getFriendSignalRConnection } from "../services/signalrService";
+import { getCookie } from "../utils/cookie";
+import { startFriendSignalRConnection } from "../services/signalrService";
 
 export const useFriendSignalR = ({ refreshFriendships }) => {
     useEffect(() => {
-        console.log("Friend hook mounted");
+        let conn;
 
-        const conn = getFriendSignalRConnection();
+        const init = async () => {
+            const token = getCookie("risen_token");
+            if (!token) return;
 
-        console.log(conn);
+            conn = await startFriendSignalRConnection(token);
 
-
-        if (!conn) return;
-
-
-
-        const refresh = async () => {
-            await refreshFriendships();
+            conn.on("FriendRequestReceived", refreshFriendships);
+            conn.on("FriendRequestAccepted", refreshFriendships);
+            conn.on("FriendRequestRejected", refreshFriendships);
+            conn.on("FriendRemoved", refreshFriendships);
         };
 
-        conn.on("FriendRequestReceived", refresh);
-        conn.on("FriendRequestAccepted", refresh);
-        conn.on("FriendRequestRejected", refresh);
-        conn.on("FriendRemoved", refresh);
+        init();
 
         return () => {
-            conn.off("FriendRequestReceived", refresh);
-            conn.off("FriendRequestAccepted", refresh);
-            conn.off("FriendRequestRejected", refresh);
-            conn.off("FriendRemoved", refresh);
+            if (conn) {
+                conn.off("FriendRequestReceived", refreshFriendships);
+                conn.off("FriendRequestAccepted", refreshFriendships);
+                conn.off("FriendRequestRejected", refreshFriendships);
+                conn.off("FriendRemoved", refreshFriendships);
+            }
         };
     }, [refreshFriendships]);
 };
